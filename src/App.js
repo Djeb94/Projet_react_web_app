5f635cbedb701046d167e36e6aae985f064d6d2f
Mathieu9488 <mathieucrespin94@gmail.com>
2024-03-26 16:38:51 +0100

    ajout du form sur la page d'accueil avec du css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Définition des états pour les valeurs des champs du formulaire
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  // Fonction de soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Empêche le rechargement de la page lors de la soumission du formulaire
+    // Vous pouvez traiter les valeurs des champs ici
+    console.log("Password:", password);
+    console.log("Email:", email);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Formulaire */}
+        <form onSubmit={handleSubmit}>
+          {/* Champ de saisie pour le nom */}
+          <br />
+          {/* Champ de saisie pour l'email */}
+          <label>
+            Email:
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <br />
+          {/* Bouton de soumission */}
+          <button type="submit">Soumettre</button>
+        </form>
       </header>
     </div>
   );
