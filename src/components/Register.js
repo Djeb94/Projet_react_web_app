d2bf33dd0e6e011d2027e0d1ea62f9d82826d894
Djeb94 <151559648+Djeb94@users.noreply.github.com>
2024-03-27 09:55:25 +0100

    css fix

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-//import './App.css';
+import '../App.css';
 
 function Login() { // Renommez App en Login
   const [password, setPassword] = useState(''); 
